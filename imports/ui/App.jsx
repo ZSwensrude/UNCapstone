fce680a3b11169fa00f8ab13c6b625ed4279601a
Zachary Swensrude <swensrudez@gmail.com>
2024-01-03 19:34:35 -0700

    install mui :)

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
+import { Typography } from '@mui/material';
 
 export const App = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <Typography>Test</Typography>
     <Hello/>
     <Info/>
   </div>
