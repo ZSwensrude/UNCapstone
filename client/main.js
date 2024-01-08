17419ba770f04e123da5f7b1b8a088ae72761772
Zachary Swensrude <swensrudez@gmail.com>
2024-01-07 17:52:40 -0700

    cleanup default pages, set up routing

--- client/main.js
+++ client/main.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
 import '@mui/material/styles';
 import theme from './theme.jsx'; // Import your custom theme
 import { ThemeProvider } from '@emotion/react';
+import { Route, Link, Routes, BrowserRouter } from 'react-router-dom';
+
+// import the main pages we need
+import Home from '../pages/Home.js';
 
 function Main() {
   return (
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
