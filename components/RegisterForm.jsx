7a364541ee8d1852133ddfe47bf580405f6535d3
Brant-Harker <brant.harker12@gmail.com>
2024-01-23 17:56:37 -0700

    Navigation works

---
+++ components/RegisterForm.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import { diasCollection } from '/imports/api/dias';
+import { useNavigate } from 'react-router-dom';
+import { redirect } from "react-router-dom";
+
+export const RegisterForm = () => {
+    const [username, setUsername] = useState("");
+    const [password1, setPassword1] = useState("");
+    const [password2, setPassword2] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = e => {
+        
+        e.preventDefault();
+
+        if ((!username || !password1 || !password2) || password1 !== password2) return;
+    
+        diasCollection.insert({
+          user: username.trim(),
+          pass: password1.trim()
+        });
+        
+        navigate('/dias-home-page');
+      };
+
+    return (
+        <form className="regContainer" onSubmit={handleSubmit}>
+            <h1 className="regHeader1">Register</h1>
+            <div className="inputRegister">
+                <h6 className="regHeader6">Username</h6> 
+                <input className="inputBoxRegister" 
+                type="text" 
+                required
+                value = {username}
+                onChange={(e) => setUsername(e.target.value)}
+                 />
+            </div>
+            <div className="inputRegister">
+                <h6 className="regHeader6">Password</h6>
+                <input className="inputBoxRegister" 
+                type="password" 
+                required 
+                value = {password1}
+                onChange={(e) => setPassword1(e.target.value)}/>
+            </div>
+            <div className="inputRegister">
+                <h6 className="regHeader6">Re-Enter Password</h6>
+                <input className="inputBoxRegister" 
+                type="password" 
+                required 
+                value = {password2}
+                onChange={(e) => setPassword2(e.target.value)}/>
+            </div>
+            <button className="buttonRegister" type="submit">Create Account</button>
+        </form>
+    );
+};
--- pages/Register.js
+++ pages/Register.js
@@ -1,27 +1,12 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import './Register.css';
+import { RegisterForm } from "../components/RegisterForm";
 
 // Placeholder for Dias screen
 const Register = () => {
   return (
-    
-        <div className="regContainer">
-            <h1 className="regHeader1">Register</h1>
-            <div className="inputRegister">
-                <h6 className="regHeader6">Username</h6> 
-                <input className="inputBoxRegister" type="text" required />
-            </div>
-            <div className="inputRegister">
-                <h6 className="regHeader6">Password</h6>
-                <input className="inputBoxRegister" type="password" required />
-            </div>
-            <div className="inputRegister">
-                <h6 className="regHeader6">Re-Enter Password</h6>
-                <input className="inputBoxRegister" type="password" required />
-            </div>
-            <button className="buttonRegister" type="submit">Create Account</button>
-        </div>
+    <RegisterForm />
     
   );
 }
