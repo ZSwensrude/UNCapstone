b32e23c9b8b3e5f5d15d7941967a01f836d70679
Zachary Swensrude <swensrudez@gmail.com>
2024-01-31 17:05:49 -0700

    set up notification menu

--- pages/Delegate.js
+++ pages/Delegate.js
@@ -8,13 +8,21 @@ import DelegateToggle from "../components/DelegateToggle";
 import CurrentMotion from "../components/CurrentMotion";
 import MessageDias from "../components/MessageDias";
 import WorkingGroupsList from "../components/WorkingGroupsList";
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { Paper, Typography } from "@mui/material";
 
 // Placeholder for delegate screen
 const Delegate = () => {
   const [formal, setFormal] = useState(false);
   const [motion, setMotion] = useState({});
+  const [openNotification, setOpenNotification] = useState(false);
 
-  const ToggleClick = () => {
+  const notificationClick = () => {
+    setOpenNotification(!openNotification);
+    console.log('clicked notifications')
+  };
+
+  const toggleClick = () => {
     setFormal(!formal);
     console.log("formal", formal);
   };
@@ -25,7 +33,7 @@ const Delegate = () => {
       <div id="main">
         {/* <Typography variant="h1">Delegate</Typography> */}
         <div id="toggleButton">
-          <DelegateToggle formal={formal} onClick={ToggleClick}/>
+          <DelegateToggle formal={formal} onClick={toggleClick}/>
         </div>
         { formal ? (
           // this will be the formal delegate dashboard
@@ -45,6 +53,16 @@ const Delegate = () => {
           // and here is the informal
           <>
             <WorkingGroupsList />
+            <div className="notifications">
+              <NotificationsIcon className="notifIcon" onClick={notificationClick}/>
+            </div>
+            <div className="notification" style={{zIndex:'1'}} >
+              { openNotification && (
+                <Paper className="notificationContent">
+                  <Typography>longer test sentence here</Typography>
+                </Paper>
+              )}
+            </div>
           </>
         )}
       </div>
