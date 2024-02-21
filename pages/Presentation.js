48a00a39b266e872a4f061464217b6dfc37a5040
Zachary Swensrude <swensrudez@gmail.com>
2024-02-21 14:37:30 -0700

    formal presentation done

--- pages/Presentation.js
+++ pages/Presentation.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useTracker } from 'meteor/react-meteor-data';
-import { Typography } from "@mui/material";
+import { Paper, Typography } from "@mui/material";
 import Header from "../components/Header";
 import './presentation.css';
 import { conferenceCollection } from "../imports/api/conference";
 import SpeakersList from "../components/SpeakersList";
 import CurrentSpeaker from "../components/CurrentSpeaker";
+import { motionCollection } from "../imports/api/motions.js";
+import MotionsDias from "../components/MotionsDias";
 
 const Presentation = () => {
   const [status, setStatus] = useState(true);
@@ -20,6 +22,13 @@ const Presentation = () => {
     }
   }, []);
 
+  const { motionsListDias = [] } = useTracker(() => {
+    const handler = Meteor.subscribe('motions');
+    const motionsListDias = motionCollection.find().fetch();
+    activeMotion = motionCollection.find({ active: true }).fetch();
+    return { motionsListDias };
+  });
+
   useEffect( () => {
     console.log("status", status);
   }, [status]);
@@ -29,12 +38,21 @@ const Presentation = () => {
       <Header version={'blank'}/>
       <div className="welcomeHeader1">
         <img src={window.location.origin + '/images/UN_emblem_blue.png'} height={100} alt="logoImage" />
-        <Typography variant="h6">Welcome to Mac-UN!</Typography>
+        <Typography style={{ marginTop:'-10px' }} variant="h6">Welcome to Mac-UN!</Typography>
       </div>
       { status === 'formal' ? (
         <div className="presentationBody">
           <SpeakersList blank={true} />
           <CurrentSpeaker confCode={code} />
+
+          <Paper id='presentationMotions'>
+            <Typography style={{ marginBottom:'20px' }} variant="h4">Current Motions:</Typography>
+            {motionsListDias &&
+              motionsListDias.map((aMotionDias, index) => (
+                <MotionsDias blank={true} key={`${index}motions`} aMotionDias={aMotionDias} /> 
+              ))
+            }
+          </Paper>
         </div>
       ) : (
         <div className="presentationBody">   
